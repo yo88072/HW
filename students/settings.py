f357c83ef8760436bfa634c5dda2f0e972144f0e
yo88072 <62133498+yo88072@users.noreply.github.com>
2020-04-25 22:04:11 +0800

    Update settings.py

--- students/settings.py
+++ students/settings.py
@@ -35,9 +35,9 @@ from linebot.models import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'Ub8e3cf75739079f25a50f82b2cbd4c63'
 # 必須放上自己的Channel Access Token
-LINE_CHANNEL_ACCESS_TOKEN ='528gEzSdPX8Pw9mTJlfpXRpTBdETmaT0LJuHs8+3/qBsiz1lWzBRIfWXQexgOdiPyGxeLtL9HAggCjAp0LBJcqKAdv7h8Ux5JQpKMioyobyYoBQpwer/eA3cA1e89/b80sl219M34lvpXlAAFud++wdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_ACCESS_TOKEN ='U/oxnEEfJLEwZFEtqBH++750v7GudzuUiIOnuHGhZYJB3UBs3w1php0g9p2aQysS91JiqCvLa+NGAIoNbsAYrUtwnLzMjjXj6pkZpc++S6JKP6RWHXa7LIMShisHvURPruJe8mGgzwguGS68b3XTfQdB04t89/1O/w1cDnyilFU='
 # 必須放上自己的Channel Secret
-LINE_CHANNEL_SECRET='43664ae683e1af5eb58087c85b74216d'
+LINE_CHANNEL_SECRET='0f18ba714a643fd680ff0a01bc068975'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
